a19acf4d34b3b0f5d3e3f70992e08f85d73b119c
adriangonc <adriangonc@yahoo.com.br>
2023-06-19 14:33:12 -0300

    Simple Flux and Mono examples.

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,35 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
 public class FluxAndMonoGeneratorService {
+
+    public static void main(String[] args){
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+        fluxAndMonoGeneratorService.fluxOfNames().subscribe(
+                customer -> {
+                    System.out.println("Customer flux names: " + customer);
+                }
+        );
+
+        fluxAndMonoGeneratorService.customerMono().subscribe(
+                customer -> {
+                    System.out.println("Customer mono name is: " + customer);
+                }
+        );
+    }
+
+    public Flux<String> fluxOfNames() {
+        var customerMap = Map.of("Adriano", 1988, "Sara", 1986, "Maria", 1970);
+        return Flux.fromIterable(customerMap.keySet());
+    }
+
+    public Mono<String> customerMono(){
+        return Mono.just("Adriano");
+    }
+
 }
