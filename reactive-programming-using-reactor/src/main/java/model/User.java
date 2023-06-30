a205dc5011bfb08142793b6a8c0860b32c4ae341
adriangonc <adriangonc@yahoo.com.br>
2023-06-30 17:32:22 -0300

    Creation of User model

---
+++ reactive-programming-using-reactor/src/main/java/model/User.java
@@ -0,0 +1,70 @@
+package model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class User {
+
+    private String id;
+    private String name;
+
+    private Boolean active;
+
+    private LocalDate birthDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(active, user.active) && Objects.equals(birthDate, user.birthDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, active, birthDate);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", active=" + active +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+
+}
